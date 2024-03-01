1a8924736d99bc5fd3f89c931801a852c0eb2b3c
Alisher Mavlonov <amavlonoffs@gmail.com>
2024-03-01 16:50:21 +0500

    Finished 4 section!!!!

--- Client/Services/CartService/CartService.cs
+++ Client/Services/CartService/CartService.cs
@@ -5,10 +5,12 @@ namespace RazorShop.Client.Services.CartService;
 public class CartService : ICartService
 {
     private readonly ILocalStorageService _localStorageService;
+    private readonly HttpClient _http;
 
-    public CartService(ILocalStorageService localStorageService)
+    public CartService(ILocalStorageService localStorageService, HttpClient http)
     {
         _localStorageService = localStorageService;
+        _http = http;
     }
     
     public event Action? OnChange;
@@ -19,9 +21,17 @@ public class CartService : ICartService
         {
             cart = new List<CartItem>();
         }
-        
-        cart.Add(cartItem);
 
+        var sameItem = cart.Find(x => x.ProductId == cartItem.ProductId
+                                      && x.ProductTypeId == cartItem.ProductTypeId);
+        if (sameItem == null)
+        {
+            cart.Add(cartItem);
+        }
+        else
+        {
+            sameItem.Quantity += cartItem.Quantity;
+        }
         await _localStorageService.SetItemAsync("cart", cart);
         OnChange.Invoke();
     }
@@ -35,5 +45,55 @@ public class CartService : ICartService
         }
 
         return cart;
+    }
+
+    public async Task<List<CartProductResponse>> GetCartProducts()
+    {
+        var cartItems = await _localStorageService.GetItemAsync<List<CartItem>>("cart");
+        var response = await _http.PostAsJsonAsync("api/cart/products", cartItems);
+
+        var cartProducts =
+            await response.Content.ReadFromJsonAsync<ServiceResponse<List<CartProductResponse>>>();
+
+        return cartProducts.Data;
+
+    }
+
+    public async Task RemoveProductFromCart(int productId, int productTypeId)
+    {
+        var cart = await _localStorageService.GetItemAsync<List<CartItem>>("cart");
+        if (cart == null)
+        {
+            return;
+        }
+
+        var cartItem = cart.Find(x => x.ProductId == productId
+                                      && x.ProductTypeId == productTypeId);
+        if (cartItem != null)
+        {
+            cart.Remove(cartItem);
+            await _localStorageService.SetItemAsync("cart", cart);
+            OnChange?.Invoke();
+        }
+        
+    }
+
+    public async Task UpdateQuantity(CartProductResponse product)
+    {
+        var cart = await _localStorageService.GetItemAsync<List<CartItem>>("cart");
+        if (cart == null)
+        {
+            return;
+        }
+
+        var cartItem = cart.Find(x => x.ProductId == product.ProductId
+                                      &&
+                                      x.ProductTypeId == product.ProductTypeId);
+        if (cartItem != null)
+        {
+            cartItem.Quantity = product.Quantity;
+            await _localStorageService.SetItemAsync("cart", cart);
+        }
+
     }
 }
